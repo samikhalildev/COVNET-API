796cf52b0bfde56c6de588b80751babdee5b32ce
samikhalildev <sami.khalil.dev@gmail.com>
2020-04-15 16:26:31 +1000

    fix prod config issue

--- config/config.js
+++ config/config.js
@@ -1,13 +1,5 @@
-let DB_URI;
-
-if (process.env.NODE_ENV === 'production') {
-  DB_URI = process.env.MONGO_URI;
-} else {
-  DB_URI = 'mongodb://localhost/covnet';
-}
-
 module.exports = {
   port: process.env.PORT || 5000,
-  database: DB_URI,
+  database: process.env.MONGODB_URI || 'mongodb://localhost/covnet',
   secretOrKey: '#$k92jR324ew2131f2FL@$22'
 };
